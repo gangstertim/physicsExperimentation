7824394f98e575fa1156730f17009e1a272e691d
Tim <timmil@50onred.com>
2014-10-24 00:41:11 -0400

    added images and pre-game stuff

--- scripts/physics.js
+++ scripts/physics.js
@@ -1,14 +1,49 @@
-//http://jsfiddle.net/Razor/PaRCj/4/
+document.body.className = 'before-game';
+var inGame = false;
+var par = parent;
+var world = null;
+var renderer = Physics.renderer('canvas', {
+    el: 'viewport', // id of the canvas element
+    width: par.innerWidth,
+    height: par.innerHeight
+});
 
-Physics(function(world){
+//capture canvas dimensions
+try {
+  par && par.innerWidth;
+} catch( e ){
+  par = window;
+}
+document.addEventListener('keydown', function( e ){
+  if (!inGame && e.keyCode === 90){
+    document.body.className = 'in-game';
+    inGame = true;
+    newGame();
+  }
+});
+window.addEventListener('resize', function(){
+    renderer.el.width = par.innerWidth;
+    renderer.el.height = par.innerHeight;
+});
+  
+function newGame() { 
+  if (world){
+    world.destroy();
+  }
+  world = Physics( init );
+};
+
+
+
+var init = function init( world, Physics ){
 	/************* Parameters ************/
-	var CANVAS_WIDTH = 500, CANVAS_HEIGHT = 500;
-	var bounds = Physics.aabb(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-	var renderer = Physics.renderer('canvas', {
-	    el: 'viewport', // id of the canvas element
-	    width: CANVAS_WIDTH,
-	    height: CANVAS_HEIGHT
-	});
+	var bounds = Physics.aabb(0, 0, par.innerWidth, par.innerHeight);
+	// var renderer = Physics.renderer('dom', {
+ //        el: 'viewport',
+ //        width: CANVAS_WIDTH,
+ //        height: CANVAS_HEIGHT,
+ //        meta: false, // don't display meta data
+ //    });
 	var colors = ['#1BA1E2', '#A05000', '#339933',
 				  '#A2C139', '#D80073', '#F09609',
 				  '#E671B8', '#A200FF', '#E51400',
@@ -50,27 +85,31 @@ Physics(function(world){
 			},
 			dist: el.dist
 		});
+		dNode.view = new Image();
+		dNode.view.src = 'images/' + i + '.png';
 		nodes.push(dNode);
 	});
 	var userNode = Physics.body('circle', {
-		x: CANVAS_WIDTH/2,
-		y: CANVAS_HEIGHT/2,
+		x: par.innerWidth/2,
+		y: par.innerHeight/2,
 		radius: 50,
 		treatment: "static",
 		styles: {
 			fillStyle: colors[5]
 		}
 	});
+	userNode.view = new Image();
+  userNode.view.src = 'images/userNode.png';
 
 	/*************** Behaviors *************/
 	var edgeCollision = Physics.behavior('edge-collision-detection', {
 	    aabb: bounds,
-	    restitution: 0.3
+	    restitution: 0.8
 	});
 	var bodyImpulseResponse = Physics.behavior('body-impulse-response');
 	var bodyCollisionDetection = Physics.behavior('body-collision-detection');
 	var attractor = Physics.behavior('attractor', {
-		pos: {'x': CANVAS_HEIGHT/2, 'y': CANVAS_WIDTH/2},
+		pos: {'x': par.innerWidth/2, 'y': par.innerHeight/2},
 		strength: 5,
 		max: false, //infinite
 		min: 100
@@ -92,7 +131,6 @@ Physics(function(world){
 	world.add( bodyImpulseResponse );
 	world.add( attractor );
 	world.add( interactions );
-	world.add( renderer );
 	//world.add( edgeCollision );
 	world.add( bodyCollisionDetection );
 	world.add( Physics.behavior('sweep-prune'));
@@ -103,7 +141,8 @@ Physics(function(world){
 		world.add( nodes[node] );
 	}
 
+	world.add( renderer );
 	world.on('step', function(){
     	world.render();
 	});
-});
+};
